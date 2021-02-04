1029aa17ee336104f399c5bb0ff535a50f08a8b9
olga <itsglimerry@gmail.com>
2021-02-04 14:03:06 +0000

    .

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,29 @@
+
 class DockingStation
-  def release_bike
+
+  def initialize
+    @storage = []
   end
+
+  def release_bike
+    if @storage == []
+      raise RuntimeError.new('NoBikesError')
+    end
+
+  end
+
+ def storage
+   @storage
+ end
+
+ def dock_bike(bike)
+
+   if @storage.count >= 1
+      fail('NoBikesError')
+   end
+
+   @storage << bike
+   'Bike docked!'
+ end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,24 @@
 require 'docking_station'
+require 'bike.rb'
+
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
+
+
+it 'releases the bike' do
+docking_station = DockingStation.new
+
+expect { docking_station.release_bike}.to raise_error('NoBikesError')
+# expect(docking_station.release_bike.working?).to eq true
+end
+
+it 'dock the bike' do
+ docking_station = DockingStation.new
+
+ expect(docking_station.dock_bike('bike')).to eq 'Bike docked!'
+ expect(docking_station.dock_bike).to raise_error('FullDockingStation')
+end
+
 
 end
